45bd1002d08fd551c3bd4f2a27a357b308f3e43c
NhanHC-dev <nhanhc.dev@gmail.com>
2022-06-08 13:38:07 +0700

    Add files via upload

---
+++ CupcakeCorner/Cupcake_cornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_cornerApp.swift
+//  Cupcake corner
+//
+//  Created by Hoàng Chí Nhân on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_cornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
